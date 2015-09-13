6e9205aeb6ce87647011d4481f03d35bc9d6c15a
Brigette Eckert <brigette.eckert@live.com>
2015-09-12 17:04:59 -0700

    added license

--- js/controllers/RaceController.js
+++ js/controllers/RaceController.js
@@ -1,8 +1,7 @@
 app.controller('RaceController', ['$scope', '$races', '$routeParams', function($scope, $races, $routeParams) {
 		$races.success(function(data){
 		$scope.race = data[$routeParams.raceid];
-		console.log($scope.race);
-		console.log($scope.race.races[2])
+		console.log($scope.race.races)
 
 	});
 }]);
